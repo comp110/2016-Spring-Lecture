5373e88014de5e28044dbca27c0241cc0e2247eb
Kris Jordan <krisjordan@gmail.com>
2016-01-27 12:07:39 -0500

    Lecture 4 classroom materials!

---
+++ src/comp110/lecture04/Calculator.java
@@ -0,0 +1,20 @@
+package comp110.lecture04;
+
+class Calculator {
+
+  int add(int x, int y) {
+    return x + y;
+  }
+
+  int divide(int x, int y) {
+    return x / y;
+  }
+
+  int multiply(int x, int y) {
+    System.out.println("multiply");
+    return x * y;
+  }
+
+  // Implement remainder here...
+
+}
---
+++ src/comp110/lecture04/CalculatorRunner.java
@@ -0,0 +1,12 @@
+package comp110.lecture04;
+
+// Run this file first, then open the Calculator class.
+public class CalculatorRunner extends CalculatorGUIBase {
+
+  public static void main(String[] args) {
+    // Don't write code here :) This is just the magical runner.
+    launch(args);
+    // Don't write code here :) This is just the magical runner.
+  }
+
+}
