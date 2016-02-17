429c7b9961656c8afbf8a934ce563ccc13fec581
Kris Jordan <krisjordan@gmail.com>
2016-02-17 12:04:02 -0500

    Update lecture 10.

--- src/comp110/lecture10/Sandbox.java
+++ src/comp110/lecture10/Sandbox.java
@@ -2,8 +2,34 @@ package comp110.lecture10;
 
 class Sandbox {
 
-  Player paigeTonight() {
-    return new Player();
+  String ifExample(int input) {
+    if (input > 10) {
+      if (input == 10) {
+        return "GO";
+      } else {
+        return "HEELS";
+      }
+    } else {
+      if (input == 100) {
+        return "BEAT";
+      } else {
+        if (input < 5) {
+          return "DUKE";
+        } else {
+          return "TONIGHT";
+        }
+      }
+    }
+  }
+
+  int whileExample() {
+    int count = 0;
+    int i = 0;
+    while (i < 4) {
+      count++;
+      i = i + 2;
+    }
+    return count;
   }
 
 }
