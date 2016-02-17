f9cfdf9b804c5a6a15961b66756cdddcdf9ed17d
Kris Jordan <krisjordan@gmail.com>
2016-02-17 11:24:32 -0500

    Support for lecture10.

---
+++ src/comp110/lecture10/Sandbox.java
@@ -0,0 +1,9 @@
+package comp110.lecture10;
+
+class Sandbox {
+
+  Player paigeTonight() {
+    return new Player();
+  }
+
+}
---
+++ src/comp110/lecture10/SandboxRunner.java
@@ -0,0 +1,12 @@
+package comp110.lecture10;
+
+public class SandboxRunner extends ClassGalleryApp {
+
+  public static void main(String[] args) {
+
+    _instance = new Sandbox();
+    launch(args);
+
+  }
+
+}
