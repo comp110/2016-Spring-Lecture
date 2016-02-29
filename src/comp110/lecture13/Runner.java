6483fdcde902753477c6f17f88e1d13c28e2dca2
Kris Jordan <krisjordan@gmail.com>
2016-02-29 11:39:42 -0500

    L13 tweaks.

--- src/comp110/lecture13/Runner.java
+++ src/comp110/lecture13/Runner.java
@@ -1,6 +1,6 @@
 package comp110.lecture13;
 
-public class SandboxRunner extends ClassGalleryApp {
+public class Runner extends ClassGalleryApp {
   public static void main(String[] args) {
 
     _instance = new CDSData();
